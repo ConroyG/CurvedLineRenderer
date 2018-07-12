a066ce341e87efc3ad4c9963628007b20fc63257
Conroy Griffiths <conroygriffiths@windowslive.com>
2018-07-12 14:58:37 +0100

    Initial Commit

---
+++ Assets/Scripts/CurveObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( menuName =  "CurveObject")]
+public class CurveObject : ScriptableObject
+{
+
+    [SerializeField] private AnimationCurve _curve;
+
+    public AnimationCurve Curve
+    {
+        get { return _curve; }
+        set { _curve = value; }
+    }
+}
---
+++ Assets/Scripts/CurveRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class CurveRenderer : MonoBehaviour
+{
+    [SerializeField] private CurveObject _curveObject;
+
+    [SerializeField] private float _width = 10;
+    [SerializeField] private float _height = 10;
+    [SerializeField] private float _sampleStep = 0.1f;
+
+    private LineRenderer _lineRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+
+
+        RenderCurve();
+    }
+
+    private void RenderCurve()
+    {
+        var numOfSteps = (int) (1 / _sampleStep) + 1;
+
+        _lineRenderer.positionCount = numOfSteps;
+
+        for (int i = 0; i < numOfSteps; i++)
+        {
+            var sampleValue = _curveObject.Curve.Evaluate(((float) i) * _sampleStep);
+
+            _lineRenderer.SetPosition(i, new Vector3((i / (float)numOfSteps) * _width, sampleValue * _height, 0));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
